139e4799604b48396eca47c99b4e2274123482f2
apobits <apobits@gmail.com>
2017-07-18 17:58:39 -0500

    commit

---
+++ WebServices/src/ortiz/perez/albin/ws/test/WSCaller.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package ortiz.perez.albin.ws.test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * @author Administrador
+ *
+ */
+public class WSCaller {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		try {
+//			URL url = new URL(args[0]);
+//		} catch (MalformedURLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		System.out.println(1 << 2);
+	}
+
+}
