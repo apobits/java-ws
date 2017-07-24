5f74b43ba85885c31f6788757096b1fef4913920
aposo <apobits@gmail.com>
2017-07-24 17:26:12 -0500

    commit

--- WebServices/src/ortiz/perez/albin/ws/test/WSCaller.java
+++ WebServices/src/ortiz/perez/albin/ws/test/WSCaller.java
@@ -3,7 +3,10 @@
  */
 package ortiz.perez.albin.ws.test;
 
-import java.net.MalformedURLException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.net.URL;
 
 /**
@@ -17,13 +20,36 @@ public class WSCaller {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		try {
-//			URL url = new URL(args[0]);
-//		} catch (MalformedURLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		System.out.println(1 << 2);
+		try {
+			URL url = new URL(args[0]);
+			Socket socket = new Socket(url.getHost(), url.getPort());
+			StringBuilder sb = new StringBuilder();
+			sb.append("Get ");
+			sb.append(url.getPath());
+			sb.append(" HTTP/1.1\n");
+			sb.append("Host: ");
+			sb.append(url.getHost());
+			sb.append(":");
+			sb.append(url.getPort());
+			sb.append("\n\n");
+
+			PrintWriter pw = new PrintWriter(socket.getOutputStream());
+			pw.print(sb.toString());
+			pw.flush();
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+
+			socket.close();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
---
+++ WebServices/src/ortiz/perez/albin/ws/test/WSCaller1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ortiz.perez.albin.ws.test;
+
+/**
+ * @author aposo
+ *
+ */
+public class WSCaller1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
